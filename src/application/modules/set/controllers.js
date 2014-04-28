1b139edabb645d2ef07a951a4087cb9613362b03
Kirstla Allred <kirstlaj@gmail.com>
2014-04-28 06:20:08 -0600

    Update controllers.js

--- src/application/modules/set/controllers.js
+++ src/application/modules/set/controllers.js
@@ -1,1 +1,14 @@
 angular.module('application')
+.controller('WorkOutSetsController',[
+	'workoutSetService',
+	$log,
+	$scope,
+	function (Service,$log,$scope) {
+		service.fetchAll()
+		.then(function (sets) {
+			$scope.sets = sets;	
+		}function (exception) {
+			$log.debug(exception);
+		})
+	}
+]);
