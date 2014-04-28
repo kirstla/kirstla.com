bfc505cd1c85e30651cb0c515be6cc2fcd2d8f71
Kirstla Allred <kirstlaj@gmail.com>
2014-04-28 16:52:59 -0600

    delete this file

--- src/application/modules/set/controllers.js
+++ src/application/modules/set/controllers.js
@@ -1,14 +1,1 @@
-angular.module('application')
-.controller('WorkOutSetsController',[
-	'workoutSetService',
-	$log,
-	$scope,
-	function (Service,$log,$scope) {
-		service.fetchAll()
-		.then(function (sets) {
-			$scope.sets = sets;	
-		}function (exception) {
-			$log.debug(exception);
-		})
-	}
-]);
+//
